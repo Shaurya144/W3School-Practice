61b969213a72ca696d0bb1e12efa9c7303117bbd
Shaurya144 <chundawatshaurya@gmail.com>
2023-09-29 19:56:57 +0100

    Add files via upload

---
+++ GetStarted.py
@@ -0,0 +1,21 @@
+print("Hello world!") # prints hello world onto terminal
+
+# There are two windows in python IDLE
+# IDLE is an IDE (Integrated Developement Environment)
+# It is used to edit and output your code 
+# A good example of IDEs is VS Code 
+# These IDEs usually have two windows
+# The Editor window and the Shell window
+# The Editor window is where you "edit" and save your code
+# Whereas the shell window can be used to write and output your code it does not save your code 
+# So all your variables and code is lost once you close it 
+# The Shell window does have a use though, as it can be used to test/run small snippets of your code
+# To make sure it is running well
+
+# An important feature in Python is indentaton
+if 2 > 5:
+    print("This is false")
+#^^^ This is indentation it is used so that python knows when and where your "if" statements and "while" loops 
+# start and end,  They also make your code easier to read or understand for both you to read back and check
+# but for other people reviewing your code.
+# Indentation in pyton is usually four spaces or 1 tab but they can be anything you like as long as they are consistent 
---
+++ PythonINTRO.py
@@ -0,0 +1,16 @@
+"""
+What Is Python?
+
+- It can be used for web developement, software developement, 
+- Web applications, alongside software to create workflows, read and modify files
+- Maths
+- High level programming language - (uses words close to real words "if" or "while") # English language
+- easy to understand ^
+- Newest version 3
+- Basic Beginner language
+
+
+"""
+
+# ^ this is a multiline comment
+# you can also use # to make single line comments
